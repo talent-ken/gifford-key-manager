b2fb8bfcf62c55e12b28aa6180b018bb85a36e9e
Ken Abe <kenabe.talent@gmail.com>
2024-05-30 10:07:36 -0800

    Add new test endpoint

---
+++ api/test.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const server = express();
+
+server.use(express.json());
+server.use(bodyParser.json({}));
+server.use(cors());
+
+server.post("/api/express", (_, res) => {
+  res.status(200).json({ message: "Hello from Express!" });
+});
+
+module.exports = (req: any, res: any) => {
+  server(req, res);
+};
