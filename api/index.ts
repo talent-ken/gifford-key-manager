7df8ea72e97b808328e53eaffb46fbc1b67f46a9
Ken Abe <kenabe.talent@gmail.com>
2024-05-30 10:31:50 -0800

    Troubleshooting message

--- api/index.ts
+++ api/index.ts
@@ -28,6 +28,8 @@ server.get("/api", (_, res) => {
 server.post("/api", async (req, res) => {
   try {
     const message = req.body.message;
+    console.log({ message });
+
     if (!message) {
       return res.status(400).json({ error: "Message param is missing!" });
     }
