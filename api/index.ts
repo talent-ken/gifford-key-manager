1d3943df24fb2ce629ad1c65f88856ae1b879a1a
Ken Abe <kenabe.talent@gmail.com>
2024-05-30 10:18:46 -0800

    Recover index endpoint

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ server.use(express.json());
 server.use(bodyParser.json({}));
 server.use(cors());
 
-server.get("/", (_, res) => {
+server.get("/api", (_, res) => {
   res.send("GIFFORD Tech API Server is running!");
 });
 
